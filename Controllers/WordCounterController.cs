78e4bbc106100e625d1d7835abb15dfd381f9c9f
David Archer <darcher@gmail.com>
2019-06-15 12:14:28 -0400

    Use concurrent dictionary, clean up code

--- Controllers/WordCounterController.cs
+++ Controllers/WordCounterController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,54 +10,93 @@ namespace WordCounter.Controllers
 	[ApiController]
 	public class WordCounterController : ControllerBase
 	{
-		private static readonly List<WordCountSubmission> data = new List<WordCountSubmission>();
+		// In a real system, this would be a call to some external database or cache
+		private static readonly ConcurrentDictionary<DateTime, WordCountSubmission> data = new ConcurrentDictionary<DateTime, WordCountSubmission>();
 
-		// GET statistics
-		[HttpGet]
-		[Route("statistics")]
+		/// <summary>
+		/// Retrieves counts for all words submitted within the last 60 seconds.
+		/// </summary>
+		[HttpGet, Route("statistics")]
 		public ActionResult<Dictionary<string, int>> GetStatistics()
 		{
-			var oneMinuteAgo = DateTime.UtcNow.AddSeconds(-60);
+			DateTime oneMinuteAgo = this.GetCurrentTime().AddSeconds(-60);
+
 			return
 				WordCounterController.data
-				.Where(x => x.SubmitDate > oneMinuteAgo)
-				.SelectMany(submission => submission.WordCounts)
+				.Where(entry => entry.Key > oneMinuteAgo)
+				.SelectMany(entry => entry.Value.WordCounts)
 				.ToLookup(wordCount => wordCount.Key, wordCount => wordCount.Value)
 				.ToDictionary(groupOfWordCounts => groupOfWordCounts.Key, groupOfWordCounts => groupOfWordCounts.Sum());
 		}
 
-		// POST submit
-		[HttpPost]
-		[Route("submit")]
+		/// <summary>
+		/// Submits words for counting.
+		/// </summary>
+		[HttpPost, Route("submit")]
 		public ActionResult SubmitWords()
 		{
+			DateTime submitDateTime = this.GetCurrentTime();
 			string text = new StreamReader(Request.Body).ReadToEnd();
-			WordCountSubmission submission = new WordCountSubmission(DateTime.UtcNow, text);
-			WordCounterController.data.Add(submission);
+
+			WordCounterController.data.AddOrUpdate(
+				key: submitDateTime,
+				addValue: new WordCountSubmission(submitDateTime, text),
+				updateValueFactory: (existingTime, existingSubmission) => { existingSubmission.AddWords(text); return existingSubmission; });
+
 			return Ok();
 		}
-	}
 
-	public class WordCountSubmission
-	{
-		public WordCountSubmission(DateTime submitDate, string text)
+		/// <summary>
+		/// Gets the current UTC time truncated to whole seconds.
+		/// </summary>
+		/// <returns>Current UTC time truncated to whole seconds.</returns>
+		private DateTime GetCurrentTime()
 		{
-			this.SubmitDate = submitDate;
-
-			Dictionary<string, int> wordCounts = new Dictionary<string, int>();
-			string[] words = text.Split(new[] { ' ' });
-
-			foreach (var word in words)
-			{
-				int count = 0;
-				wordCounts.TryGetValue(word, out count);
-				wordCounts[word] = ++count;
-			}
-
-			this.WordCounts = wordCounts;
+			return DateTime.UtcNow.AddTicks(-(DateTime.UtcNow.Ticks % TimeSpan.FromSeconds(1).Ticks));
 		}
 
-		public DateTime SubmitDate { get; set; }
-		public Dictionary<string, int> WordCounts { get; set; }
+		private class WordCountSubmission
+		{
+			public WordCountSubmission(DateTime submitDateTime, string text)
+			{
+				if (text == null) throw new ArgumentNullException(nameof(text));
+				this.WordCounts = new ConcurrentDictionary<string, int>();
+				this.AddWords(text);
+			}
+
+			public ConcurrentDictionary<string, int> WordCounts { get; private set; }
+
+			public void AddWords(string text)
+			{
+				if (text == null) throw new ArgumentNullException(nameof(text));
+
+				// TODO: read this as a stream in case we're processing the entire library of congress in a single submission
+				foreach (string word in text.Split(new[] { ' ' }))
+				{
+					this.AddWordCount(word, 1);
+				}
+			}
+
+			public void AddWords(ConcurrentDictionary<string, int> otherWordCounts)
+			{
+				if (otherWordCounts == null) throw new ArgumentNullException(nameof(otherWordCounts));
+
+				foreach (KeyValuePair<string, int> kvp in otherWordCounts)
+				{
+					this.AddWordCount(kvp.Key, kvp.Value);
+				}
+			}
+
+			private void AddWordCount(string word, int count)
+			{
+				if (word == null) throw new ArgumentNullException(nameof(word));
+				if (count <= 0) throw new ArgumentOutOfRangeException(nameof(count));
+
+				this.WordCounts.AddOrUpdate(
+					key: word,
+					addValue: count,
+					updateValueFactory: (existingWord, existingCount) => existingCount + count);
+			}
+		}
 	}
 }
